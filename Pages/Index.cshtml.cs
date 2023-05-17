9b5b056282d67337fe29fb4bf9dcfda9beeb8842
Naveed Kharadi <nkharadi@microsoft.com>
2023-05-17 14:54:42 +1000

    first commit with app config setting to be read from

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,17 +6,21 @@ namespace dotnetcoresample.Pages;
 
 public class IndexModel : PageModel
 {
+    // requires using Microsoft.Extensions.Configuration;
+    private readonly IConfiguration Configuration;
 
     public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
-    
+    public string AppName { get { return Configuration["AppName"]; } }
+
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
+        Configuration = configuration;
     }
 
     public void OnGet()
-    {        
+    {
     }
 }
